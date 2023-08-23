53020472cd1d9d05b3eca3db33558ee4effd88b9
boendermaker <elektrobong@googlemail.com>
2023-08-24 01:17:46 +0200

    style fixed

--- public/datatest/index.js
+++ public/datatest/index.js
@@ -26,7 +26,7 @@ export class WebComponentDataTest extends LitElement {
             position: absolute;
             top: 50%;
             left: 50%;
-            transform: translate(50%, 50%), scale(10);
+            transform: translate(50%, 50%) scale(10);
         }
 
         .show {
