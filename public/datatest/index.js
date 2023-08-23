5f7677001defb171b6801e945a0e2b93d8242ba5
boendermaker <elektrobong@googlemail.com>
2023-08-23 16:19:26 +0200

    dbug

--- public/datatest/index.js
+++ public/datatest/index.js
@@ -41,8 +41,8 @@ export class WebComponentDataTest extends LitElement {
 
         return html `
         <div style="width: 100%; border: 1px solid #999;">
-            ${this.data.map((dataItem) => {
-                return html `<div>${dataItem.name}</div>`
+            ${this.dataItemsTmplArray.map((tmplItem) => {
+                return tmplItem
                 }
             )}
         </div>
