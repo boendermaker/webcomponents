f8a4a7c2cf05b9b25b0883fc18d76ad7a68aacde
boendermaker <elektrobong@googlemail.com>
2023-08-24 01:31:28 +0200

    styles fixed

--- public/datatest/index.js
+++ public/datatest/index.js
@@ -23,7 +23,7 @@ export class WebComponentDataTest extends LitElement {
         }
 
         .loader {
-            position: absolute;
+            position: fixed;
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%) scale(10);
