b701c5cca738a6405f7bc88bbb9fa1f643a5675b
boendermaker <elektrobong@googlemail.com>
2023-08-29 10:52:46 +0200

    loader changed

--- public/datatest/index.js
+++ public/datatest/index.js
@@ -110,7 +110,7 @@ export class WebComponentDataTest extends LitElement {
         return html `
         <div class="loader-container">
             <div class="loader ${this.isloading ? 'show' : 'hide'}">
-                <svg width="100" height="100" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <svg width="50" height="50" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg">
                     <path fill="#000000" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
                         <animateTransform 
                         attributeName="transform" 
